66a407d014f2b333723d1d07e82e5a31d8ee14b0
suman <yes.u.man7@gmail.com>
2024-05-24 09:40:57 +0545

    scrollng effect added

--- app/components/cards/CousinesSectionCard.tsx
+++ app/components/cards/CousinesSectionCard.tsx
@@ -1,33 +1,34 @@
 import React from "react";
-
 const CousinesSectionCard = () => {
-  const cousines = [
+  const cuisines = [
     { id: "1", imagesrc: "/Images/food3.png", title: "Italian" },
     { id: "2", imagesrc: "/Images/food3.png", title: "Indian" },
     { id: "3", imagesrc: "/Images/food3.png", title: "American" },
     { id: "4", imagesrc: "/Images/food3.png", title: "Chinese" },
     { id: "5", imagesrc: "/Images/food3.png", title: "Mexican" },
     { id: "6", imagesrc: "/Images/food3.png", title: "Thai" },
-    // { id: "7", imagesrc: "/Images/food3.png", title: "French" },
-    // { id: "8", imagesrc: "/Images/food3.png", title: "Japanese" },
-    // { id: "9", imagesrc: "/Images/food3.png", title: "Greek" },
-    // { id: "10", imagesrc: "/Images/food3.png", title: "Spanish" }
+    { id: "7", imagesrc: "/Images/food3.png", title: "French" },
+    { id: "8", imagesrc: "/Images/food3.png", title: "Japanese" },
+    { id: "9", imagesrc: "/Images/food3.png", title: "Greek" },
+    { id: "10", imagesrc: "/Images/food3.png", title: "Spanish" },
   ];
 
   return (
-    <div className="flex ">
-      {cousines.map((item) => (
-        <div key={item.id} className="m-4  w-36 overflow-hidden rounded-3xl">
-          <img
-            src={item.imagesrc}
-            alt=""
-            className="h-auto w-full object-contain"
-          />
-          <div className="text-center">
-            <p>{item.title}</p>
-          </div>
+    <div className="mt-4 flex flex-col ">
+      <div className="hide-scroll-bar flex overflow-x-scroll">
+        <div className="flex flex-nowrap">
+          {cuisines.map((item) => (
+            <div key={item.id} className="inline-block px-2">
+              <div className="m-4  w-36 overflow-hidden rounded-3xl  bg-white shadow-md transition-shadow duration-300 ease-in-out hover:shadow-xl">
+                <img src={item.imagesrc} alt={item.title} className="w-full" />
+                <div className="text-center">
+                  <p>{item.title}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
--- app/components/cards/DiscountSectionCard.tsx
+++ app/components/cards/DiscountSectionCard.tsx
@@ -2,28 +2,32 @@ import React from "react";
 
 const DiscountSectionCard = () => {
   return (
-    <div className="grid grid-cols-3 ">
-      <div className="max-w-sm min-w-sm overflow-hidden bg-red-200 rounded-xl m-4">
-        <img
-          src="https://picsum.photos/500"
-          alt=""
-          className="w-full h-48 object-cover"
-        />
-      </div>
+    <div className=" mt-4 flex flex-col   ">
+      <div className="hide-scroll-bar flex overflow-x-scroll ">
+        <div className=" flex flex-nowrap ">
+          <div className="inline-block px-3">
+            <div className="h-52 w-96 max-w-sm overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 ease-in-out hover:shadow-xl">
+              <img src="/Images/food2.png" alt="" className="w-full" />
+            </div>
+          </div>
 
-      <div className="max-w-sm overflow-hidden bg-red-200 rounded-xl m-4">
-        <img
-          src="https://picsum.photos/500"
-          alt=""
-          className="w-full h-48 object-cover"
-        />
-      </div>
-      <div className="max-w-sm overflow-hidden bg-red-200 rounded-xl m-4">
-        <img
-          src="https://picsum.photos/500"
-          alt=""
-          className="w-full h-48 object-cover"
-        />
+          <div className="inline-block px-3">
+            <div className="h-52 w-96 max-w-sm overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 ease-in-out hover:shadow-xl">
+              <img src="/Images/food2.png" alt="" className="w-full" />
+            </div>
+          </div>
+
+          <div className="inline-block px-3">
+            <div className="h-52 w-96 max-w-sm overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 ease-in-out hover:shadow-xl">
+              <img src="/Images/food2.png" alt="" className="w-full" />
+            </div>
+          </div>
+          <div className="inline-block px-3">
+            <div className="h-52 w-96 max-w-sm overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 ease-in-out hover:shadow-xl">
+              <img src="/Images/food2.png" alt="" className="w-full" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
