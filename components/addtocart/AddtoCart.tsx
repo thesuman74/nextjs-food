8d5f27bb1acf7e4711abc557c65422e61420cda6
suman <yes.u.man7@gmail.com>
2024-05-29 17:07:09 +0545

    add to cart added

--- components/addtocart/AddtoCart.tsx
+++ components/addtocart/AddtoCart.tsx
@@ -6,72 +6,66 @@ const AddtoCart = async () => {
     <div>
       <div className="h-screen bg-gray-100 pt-10">
         <h1 className="mb-10 text-center text-5xl font-bold">Cart Items</h1>
-
-        <div className="w-2/3">
-          <div className="mx-auto max-w-7xl px-6 xl:px-0">
-            {/* Container for cards */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {/* Map through data to render cards */}
-              {data.map((_, index) => (
-                <div key={index} className="rounded-lg bg-white shadow-md">
-                  <div className="p-6">
-                    <img
-                      src="/Images/food1.png"
-                      alt="product-image"
-                      className="w-full rounded-lg"
-                    />
-                    <div className="mt-4">
-                      <h2 className="text-lg font-bold text-gray-900">
-                        Nike Air Max 2019
-                      </h2>
-                      <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
-                      <div className="flex justify-between items-center mt-2">
-                        <div className="flex items-center space-x-4">
-                          <p className="text-sm">259.000 ₭</p>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="h-5 w-5 cursor-pointer text-red-500 hover:text-red-700"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M6 18L18 6M6 6l12 12"
-                            />
-                          </svg>
-                        </div>
-                        <div className="flex items-center space-x-4">
-                          <span className="cursor-pointer bg-gray-200 rounded-md px-3 py-1">
-                            {" "}
-                            -{" "}
-                          </span>
-                          <input
-                            className="h-8 w-8 border bg-white text-center text-xs outline-none"
-                            type="number"
-                            value="2"
-                            min="1"
-                          />
-                          <span className="cursor-pointer bg-gray-200 rounded-md px-3 py-1">
-                            {" "}
-                            +{" "}
-                          </span>
-                        </div>
-                      </div>
+        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+          <div className="rounded-lg md:w-2/3">
+            {data.map((_, index) => (
+              <div
+                key={index}
+                className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start"
+              >
+                <img
+                  src="/Images/food1.png"
+                  alt="product-image"
+                  className="w-full rounded-lg sm:w-40"
+                />
+                <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                  <div className="mt-5 sm:mt-0">
+                    <h2 className="text-lg font-bold text-gray-900">
+                      Nike Air Max 2019
+                    </h2>
+                    <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
+                  </div>
+                  <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                    <div className="flex items-center border-gray-100">
+                      <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                        {" "}
+                        -{" "}
+                      </span>
+                      <input
+                        className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                        type="number"
+                        value="2"
+                        min="1"
+                      />
+                      <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                        {" "}
+                        +{" "}
+                      </span>
+                    </div>
+                    <div className="flex items-center space-x-4">
+                      <p className="text-sm">259.000 ₭</p>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        </div>
-
-        {/* card 1 sent */}
-        <div className="w-1/3">
-          {/* Additional content for card 2 */}
-          <div className="mt-6 rounded-lg bg-white p-6 shadow-md">
+          {/* calculate section  */}
+          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
             <div className="mb-2 flex justify-between">
               <p className="text-gray-700">Subtotal</p>
               <p className="text-gray-700">$129.99</p>
@@ -93,7 +87,6 @@ const AddtoCart = async () => {
             </button>
           </div>
         </div>
-        {/* card 2 ends  */}
       </div>
     </div>
   );
