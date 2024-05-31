991210272642419b353d9ab29cc7ae878bf173dc
suman <yes.u.man7@gmail.com>
2024-05-31 15:59:15 +0545

    data is showing on the cart

--- components/cards/PopularCategoriesCard.tsx
+++ components/cards/PopularCategoriesCard.tsx
@@ -14,39 +14,35 @@ const PopularCategoriesCard = async () => {
     console.log("Adding to cart", productId);
     dispatch(add(productId));
   };
-  // fetching data from api
+
+  // Fetching data from API
   const res = await fetch(`http://localhost:8000/deals`, {
     next: { revalidate: 10 },
   });
   const data = await res.json();
-  console.log("this is fetched from resturant ", data);
+  console.log("Fetched from restaurant:", data);
 
   return (
     <section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 container max-w-7xl mx-auto p-4">
       {data.map((item: DealsTypes) => (
         <Link href={`/deals/${item.id}`} key={item.id}>
-          <div
-            key={item.id}
-            className="relative max-w-xs mt-3 rounded-lg shadow-md mx-auto group"
-          >
-            {/* fav button  */}
+          <div className="relative max-w-xs mt-3 rounded-lg shadow-md mx-auto group">
+            {/* Favorite button */}
             <div className="w-7 h-7 z-10 flex items-center justify-center rounded-full bg-white top-2 right-5 absolute hover:fill group-hover:scale-105">
               <button onClick={() => handleAddToCart(item.id)}>
-                <Link key={item.id} href={""}>
+                <Link href={""}>
                   <Heart className="hover:fill-red-500" color="#e13370" />
                 </Link>
               </button>
             </div>
             <img
               src="/Images/food1.png"
-              alt=""
+              alt={item.title}
               className="w-full rounded-lg rounded-b-none transition-transform duration-200 group-hover:scale-105"
             />
             <div className="bg-gray-100 p-2 rounded-lg rounded-t-none">
-              <p className="ml-5 font-medium text-wrap text-sm ">
-                {item.title}
-              </p>
-              <p className="text-Ptext text-sm ml-5">21 {item.subtitle}</p>
+              <p className="ml-5 font-medium text-wrap text-sm">{item.title}</p>
+              <p className="text-Ptext text-sm ml-5">{item.subtitle}</p>
             </div>
           </div>
         </Link>
