252cdad671696f43cfa67840ff5d1545208e08e3
suman <yes.u.man7@gmail.com>
2024-06-05 19:08:43 +0545

    Google sign in added

---
+++ app/actions/authAction.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+
+export async function doSocialLogin(formData: FormData) {
+  const action = formData.get("action");
+
+  if (typeof action === "string") {
+    await signIn(action, { redirectTo: "/profile" });
+    console.log(action);
+  } else {
+    console.error("Action is not a valid string");
+  }
+}
+
+export async function doLogout() {
+  await signOut({ redirectTo: "/login" });
+}
