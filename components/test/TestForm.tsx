3b5b7a2e16e5a177848356f03f169e4a84cd3cba
suman <yes.u.man7@gmail.com>
2024-06-11 14:51:29 +0545

    tansatck query start

---
+++ components/test/TestForm.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { doCredentialLogin } from "@/app/actions/authAction";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const LoginForm = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  async function onSubmit(event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+      const email = formData.get("email");
+      const password = formData.get("password");
+
+      console.log("this is form data ", email, password);
+
+      const response = await doCredentialLogin(formData);
+
+      if (response) {
+        console.log("docredentiallogin is true");
+      }
+
+      if (!!response.error) {
+        console.error(response.error);
+        setError(response.error.message);
+      } else {
+        router.push("/profile");
+      }
+    } catch (e) {
+      console.error(e);
+      setError("Check your Credentials");
+    }
+  }
+
+  return (
+    <>
+      <div className="text-xl text-red-500">{error}</div>
+      <form
+        className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
+        onSubmit={onSubmit}
+      >
+        <div className="my-2">
+          <label htmlFor="email">Email Address</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="email"
+            name="email"
+            id="email"
+          />
+        </div>
+
+        <div className="my-2">
+          <label htmlFor="password">Password</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-orange-300 mt-4 rounded flex justify-center items-center w-36"
+        >
+          Ceredential Login
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
