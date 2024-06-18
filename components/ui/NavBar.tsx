de78972f4618945798c203e45b2813a4b342f333
suman <yes.u.man7@gmail.com>
2024-06-18 13:53:36 +0545

    Links created for navbar

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -5,6 +5,14 @@ import AddtoCartButton from "./AddtoCartButton";
 import { auth } from "@/auth";
 import Logout from "../Logout";
 
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Resturants", href: "/resturants" },
+  { name: "Menus", href: "/menus" },
+  { name: "wishlist", href: "/wishlist" },
+  { name: <AddtoCartButton />, href: "/cart" },
+];
+
 const NavBar = async () => {
   const session = await auth();
 
@@ -13,7 +21,7 @@ const NavBar = async () => {
       <nav className="w-full  z-50 bg-yellow-50  ">
         <div className="container mx-auto px-2 md:px-12 lg:px-7">
           <div className="flex items-center justify-between  gap-6 md:py-4 md:gap-0">
-            {/* item 1 */}
+            {/* item 1 logo and title */}
             <div className="flex items-center space-x-4">
               <Link
                 href="/"
@@ -33,48 +41,25 @@ const NavBar = async () => {
               </Link>
             </div>
 
-            {/* item 2 */}
+            {/* item 2 : Location */}
             <div className="flex-grow flex justify-center">
               <LocationDialog />
             </div>
 
-            {/* item 3 */}
+            {/* item 3 : navlinks */}
             <div className="hidden lg:flex items-center space-x-4">
               <div className="text-gray-600 lg:pr-4">
                 <ul className="flex space-x-4 tracking-wide font-medium text-sm">
-                  <li>
-                    <a
-                      href="/resturants"
-                      className="transition hover:text-yellow-700"
-                    >
-                      Resturants
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/menus"
-                      className="transition hover:text-yellow-700"
-                    >
-                      Menus
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/wishlist"
-                      className="transition hover:text-yellow-700"
-                    >
-                      Wishlist
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/cart"
-                      className="transition hover:text-yellow-700"
-                    >
-                      <AddtoCartButton />
-                      {/* cart */}
-                    </a>
-                  </li>
+                  {links.map((link) => (
+                    <li>
+                      <Link
+                        href={link.href}
+                        className="transition hover:text-yellow-700"
+                      >
+                        {link.name}
+                      </Link>
+                    </li>
+                  ))}
                 </ul>
               </div>
 
@@ -97,10 +82,6 @@ const NavBar = async () => {
                       </span>
                     )}
                   </div>
-
-                  {/* <span className="text-yellow-800 font-semibold text-sm">
-                    Sign up
-                  </span> */}
                 </button>
 
                 <div className="py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300">
@@ -118,11 +99,6 @@ const NavBar = async () => {
                     </Link>
                   )}
                 </div>
-                {/* <Link href={"/login"}>
-                    <span className="text-yellow-900 font-semibold text-sm">
-                      Login
-                    </span>
-                  </Link> */}
               </div>
             </div>
           </div>
